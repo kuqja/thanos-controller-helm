d19ecb42b7f9b0bbf48a7010cbe09d48f9f32191
Aleksander Dushku <aleksanderdushku@hotmail.com>
2024-04-21 21:49:52 +0000

    updated helm chart, added new configs and tests

---
+++ delete_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Uninstalling thanos receive controller chart and its resources"
+helm uninstall thanos-receive-controller  -n th-controller
+
+echo "Uninstalling thanos chart and its resources"
+helm uninstall thanos -n th-controller
+
+echo "Delete th-controller namespace"
+kubectl delete ns th-controller
+
+echo "Stopping minukube cluster" 
+minikube stop
+
+echo "Environment Clenup is complete"
